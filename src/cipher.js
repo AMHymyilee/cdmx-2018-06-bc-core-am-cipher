a384c25463559fcc79001a7f02642501c71f5ab1
AdrianaMG <hymyhaluta@gmail.com>
2018-06-08 09:36:20 -0500

    html y js mejorado

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,19 @@
 window.cipher = {
   // ...
 };
-men = document.getElementById("mensaje");
-salt = document.getElementById("num_salt");
+text = document.getElementById("mensaje");
+saltos = document.getElementById("num_salt");
 
 function cifrar(){
 
-let mensaje = men.value;
-let saltos = parseInt(salt.value);
+let mensaje = text.value;
+let offset = parseInt(saltos.value);
 let result = new Array (mensaje.lenght);
 for (i = 0; i < mensaje.length; i++) {
   let no_ascii = mensaje.charCodeAt(i);
 
   if (no_ascii >= 65 && no_ascii <= 90){
-    result [i] =  String.fromCharCode((no_ascii - 65 + saltos) % 26 + 65);
+    result [i] =  String.fromCharCode((no_ascii - 65 + offset) % 26 + 65);
     console.log(result[i]);
   }
   else if (no_ascii === 32) {
@@ -21,7 +21,7 @@ for (i = 0; i < mensaje.length; i++) {
     console.log(result[i]);
   }
   else if (no_ascii >= 97 && no_ascii <= 122) {
-    result [i] =  String.fromCharCode((no_ascii - 97 + saltos) % 26 + 97);
+    result [i] =  String.fromCharCode((no_ascii - 97 + offset) % 26 + 97);
     console.log(result[i]);
   }
 }
@@ -31,20 +31,20 @@ let imprime = document.getElementById("imprime");
  return imprime.innerHTML= sinComas.join("");
 };
 
-men = document.getElementById("mensaje");
-salt = document.getElementById("num_salt");
+text = document.getElementById("mensaje");
+saltos = document.getElementById("num_salt");
 
 function decifrar(){
 
-let mensaje = men.value;
-let saltos = parseInt(salt.value);
+let mensaje = text.value;
+let offset = (parseInt(saltos.value)%26);
 let result = new Array (mensaje.lenght);
 for (i = 0; i < mensaje.length; i++) {
-  //let result = new Array (mensaje.lenght);
+
   let no_ascii = mensaje.charCodeAt(i);
 
   if (no_ascii >= 65 && no_ascii <= 90){
-    result [i] =  String.fromCharCode ((no_ascii - 65 - saltos) % 26+65 );
+    result [i] =  String.fromCharCode ((no_ascii - 65 - offset) % 26 + 65 );
     console.log(result[i]);
   }
   else if (no_ascii === 32) {
@@ -52,7 +52,7 @@ for (i = 0; i < mensaje.length; i++) {
     console.log(result[i]);
   }
   else if (no_ascii >= 97 && no_ascii <= 122) {
-    result [i] =  String.fromCharCode (26 + [(no_ascii + 97 -saltos) % 26-97] )% 26;
+    result [i] =  String.fromCharCode (26 + (no_ascii - 97 - offset) % 26 + 97);
     console.log(result[i]);
   }
 }
